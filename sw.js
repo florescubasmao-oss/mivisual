0d32e0ba09871cb0035f25b74527fec87cb34823
florescubasmao-oss <florescubasmao@gmail.com>
2026-08-27 09:28:07 -0500

    V508 renovar cache para Checklist rapido y Actas tecnico

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* MI VISUAL V507 - Fecha/hora WIN unificada y cache segura */
-const MV339_CACHE = "mivisual-v507-actualizacion-win-20260827";
+/* MI VISUAL V508 - Checklist rapido + Actas tecnico sin descarga */
+const MV339_CACHE = "mivisual-v508-checklist-actas-20260827";
 const MV339_CORE = [
   "./",
   "./index.html",
@@ -14,6 +14,8 @@ const MV339_CORE = [
   "./js/modulos_loader.js?v=V498-CIERRE-DEFINITIVO",
   "./js/gestion_win_v505_loader.js?v=V507-GESTION-WIN",
   "./js/actualizacion_win_v507.js?v=V507-FECHA-HORA-PERU",
+  "./js/checklist_rapido_v508.js?v=V508-CHECKLIST-RAPIDO",
+  "./js/actas_tecnico_sin_descarga_v508.js?v=V508-ACTAS-TECNICO",
   "./js/mapa_cto_fix_v342.js?v=V342-MAPA-CTO",
   "./js/actas_recepcion_v343.js?v=V343-ACTAS-RECEPCION",
   "./js/actas_identidad_v344.js?v=V344-ACTAS-IDENTIDAD",
