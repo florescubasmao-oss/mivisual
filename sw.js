4c2fbbad73d6ca701593a001c8f204448c87dcc8
florescubasmao-oss <florescubasmao@gmail.com>
2026-08-28 08:22:38 -0500

    V517C9: renueva cache GAR/VTR partida actual

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* MI VISUAL V517C.8 - GAR/VTR GUARDADO UNICO · CACHE CONTROLADO */
-const MV339_CACHE = "mivisual-v517c8-garvtr-guardado-unico-20260828-1";
+/* MI VISUAL V517C.9 - GAR/VTR PARTIDA ORDEN ACTUAL · CACHE CONTROLADO */
+const MV339_CACHE = "mivisual-v517c9-garvtr-partida-actual-20260828-1";
 const MV339_CORE = [
   "./",
   "./index.html",
@@ -19,7 +19,7 @@ const MV339_CORE = [
   "./js/vtr_gar_ui_fix_v516a.js?v=V516A-ROBUSTA-20260828",
   "./js/vtr_gar_ui_fix_v516b.js?v=V516B-PREEMPTIVA-20260828",
   "./js/vtr_gar_tabs_guard_v516c.js?v=V516C-TABS-20260828",
-  "./js/vtr_gar_ux_v517b.js?v=V517C8-GUARDADO-UNICO-20260828-1",
+  "./js/vtr_gar_ux_v517b.js?v=V517C9-PARTIDA-ORDEN-ACTUAL-20260828-1",
   "./js/vtr_gar_ux_v517c3.js?v=V517C3-UX-RAPIDA-20260828-1",
   "./js/vtr_gar_legacy_assoc_v517c2a.js?v=V517C2A-LEGACY-20260828-1",
   "./js/vtr_gar_gestion_v517c2.js?v=V517C2-HISTORICO-OBSERVADO-20260828-1",
@@ -28,6 +28,7 @@ const MV339_CORE = [
   "./js/vtr_gar_usabilidad_v517c6.js?v=V517C6-USABILIDAD-20260828-1",
   "./js/vtr_gar_estabilidad_v517c7.js?v=V517C7-ESTABILIDAD-20260828-1",
   "./js/vtr_gar_guardado_unico_v517c8.js?v=V517C8-GUARDADO-UNICO-20260828-1",
+  "./js/vtr_gar_partida_actual_v517c9.js?v=V517C9-PARTIDA-ORDEN-ACTUAL-20260828-1",
   "./js/partidas_win_v505.js?v=V506-PARTIDAS-BASE",
   "./js/partidas_lote_v506.js?v=V506-LOTE",
   "./js/partidas_win_v513.js?v=V513-PARTIDAS-20260827",
