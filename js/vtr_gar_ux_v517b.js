bb44205c0232d2bc07f8605fadd1358aef3ec77e
florescubasmao-oss <florescubasmao@gmail.com>
2026-08-28 11:02:44 -0500

    V517C16.1: renovar C5 y C8 para regla bono excepción

--- js/vtr_gar_ux_v517b.js
+++ js/vtr_gar_ux_v517b.js
@@ -5,13 +5,17 @@
    2) compatibilidad historica VALIDACION_TECNICA
    3) gestion V517C.2 con historico + OBSERVADO
    4) dias entre antecedente y GAR/VTR
-   5) motivo WIN + bono excepcional
+   5) motivo WIN + evaluacion Jefatura sin registro
    6) usabilidad + ficha por GET
    7) estabilidad post-guardado + snapshot local
    8) guardado unico
    9) partida de la orden actual bajo demanda
    10) manejador unico de correccion V517C.16
    11) vista compacta V517C.12 como unica capa de acciones
+
+   Regla V517C16.1:
+   - SIN REGISTRO + NO BONO = NO BONO.
+   - SIN REGISTRO + BONO = BONO · EXCEPCION.
 
    NO carga V517C.14 / V517C.14A / V517C.15.
    NO modifica Ranking, Dashboard, Produccion ni Recableado.
@@ -42,16 +46,16 @@
     .then(()=>cargar("./js/vtr_gar_legacy_assoc_v517c2a.js?v=V517C2A-LEGACY-20260828-1","MV517C2A_LEGACY_ASSOC_OK"))
     .then(()=>cargar("./js/vtr_gar_gestion_v517c2.js?v=V517C2-HISTORICO-OBSERVADO-20260828-1","MV517C2_GARVTR_GESTION_OK"))
     .then(()=>cargar("./js/vtr_gar_antecedente_dias_v517c4.js?v=V517C4-ANTECEDENTE-DIAS-20260828-1","MV517C4_ANTECEDENTE_DIAS_OK"))
-    .then(()=>cargar("./js/vtr_gar_bono_excepcion_v517c5.js?v=V517C5-MOTIVO-BONO-EXCEPCION-20260828-1","MV517C5_BONO_EXCEPCION_OK"))
+    .then(()=>cargar("./js/vtr_gar_bono_excepcion_v517c5.js?v=V517C16-1-EVAL-SIN-REGISTRO-20260828-1","MV517C5_BONO_EXCEPCION_OK"))
     .then(()=>cargar("./js/vtr_gar_usabilidad_v517c6.js?v=V517C6-USABILIDAD-20260828-1","MV517C6_USABILIDAD_OK"))
     .then(()=>cargar("./js/vtr_gar_estabilidad_v517c7.js?v=V517C7-ESTABILIDAD-20260828-1","MV517C7_ESTABILIDAD_OK"))
-    .then(()=>cargar("./js/vtr_gar_guardado_unico_v517c8.js?v=V517C8-GUARDADO-UNICO-20260828-1","MV517C8_GUARDADO_UNICO_OK"))
+    .then(()=>cargar("./js/vtr_gar_guardado_unico_v517c8.js?v=V517C16-1-GUARDADO-REGLA-20260828-1","MV517C8_GUARDADO_UNICO_OK"))
     .then(()=>cargar("./js/vtr_gar_partida_actual_v517c9.js?v=V517C9-PARTIDA-ORDEN-ACTUAL-20260828-2","MV517C9_PARTIDA_ACTUAL_OK"))
     .then(()=>cargar("./js/vtr_gar_correccion_handler_v517c16.js?v=V517C16-CORRECCION-HANDLER-20260828-1","MV517C16_CORRECCION_HANDLER_OK"))
     .then(()=>cargar("./js/vtr_gar_compacto_v517c12.js?v=V517C16-COMPACTO-ACCIONES-20260828-1","MV517C12_COMPACTO_OK"))
     .then(()=>{
       if(typeof window.mv517c12Ejecutar==="function")window.mv517c12Ejecutar();
-      console.log("MI VISUAL V517C.16: GAR/VTR cadena unica cargada.");
+      console.log("MI VISUAL V517C.16.1: GAR/VTR cadena unica cargada.");
     })
-    .catch(e=>console.error("MI VISUAL V517C.16: error de carga",e));
+    .catch(e=>console.error("MI VISUAL V517C.16.1: error de carga",e));
 })();
